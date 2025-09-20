c232d05ce07599a918acd4d18b7f436c8588a5e4
inwe-research-group <zmamanir@unmsm.edu.pe>
2025-09-19 21:41:06 -0500

    firestore sin data radom

--- app/src/main/java/com/dsm/firebaseauth2/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/dsm/firebaseauth2/presentation/home/HomeScreen.kt
@@ -4,20 +4,28 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.dsm.firebaseauth2.ui.theme.Black
 import com.dsm.firebaseauth2.presentation.model.Artist
 
@@ -42,7 +50,10 @@ fun HomeScreen(viewModel: HomeViewModel= HomeViewModel()){
         )
         LazyRow{
             items(artists.value){
-                ArtistItem(artist = it, onItemSelected={} )
+                ArtistItem(
+                    artist = it,
+                    onItemSelected={}
+                )
             }
 
         }
@@ -60,9 +71,20 @@ fun ArtistItem(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.clickable { onItemSelected(artist) }
     ){
-        /*AsynImage(
+        AsyncImage(
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape),
+            model= artist.image,
+            contentDescription = "Artists image",
+            contentScale = ContentScale.Crop,
 
-        )*/
+        )
+        Spacer(modifier=Modifier.height(4.dp))
+        Text(
+            text= artist.name.orEmpty(),
+            color=Color.White
+        )
 
     }
 
--- app/src/main/java/com/dsm/firebaseauth2/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/dsm/firebaseauth2/presentation/home/HomeViewModel.kt
@@ -24,6 +24,7 @@ class HomeViewModel : ViewModel(){
         getArtists()
     }
 
+
     private fun getArtists(){
         viewModelScope.launch {
             val result: List<Artist> = withContext(Dispatchers.IO){
